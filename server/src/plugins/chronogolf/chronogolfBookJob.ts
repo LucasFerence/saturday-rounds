758cb46770bd8003ffafc0f002618165c445b6d0
Lucas Ference <ference.lucas@gmail.com>
2023-05-11 00:16:38 -0400

    Re-add part where job gets registered

--- server/src/plugins/chronogolf/chronogolfBookJob.ts
+++ server/src/plugins/chronogolf/chronogolfBookJob.ts
@@ -1,5 +1,6 @@
 import {Job, JobAttributesData} from 'agenda';
-import {FastifyInstance} from 'fastify';
+import {FastifyInstance, FastifyPluginAsync} from 'fastify';
+import fp from 'fastify-plugin';
 import {ScheduleDetails} from 'src/schema/chronogolf/schedule';
 import {JobDefinition} from '../agenda';
 
@@ -10,12 +11,14 @@ export class ChronogolfBookJob implements JobDefinition {
   execute(fastify: FastifyInstance, job: Job, done: () => void): void {
     console.log(`Starting service for task ${job.attrs.name}`);
 
+    // TODO: Move bookChronoGolfTime into this class and remove ChronogolfBook.ts all together
     const scheduleDetails = job.attrs.data?.details as ScheduleDetails;
     fastify.bookChronogolfTime(scheduleDetails);
 
     done();
   }
 
+  // TODO: Make this an interface method and have ScheduleDetails be a type parameter on JobDefinition
   toData(scheduleDetails: ScheduleDetails): JobAttributesData {
     return {
       type: CHRONOGOLF_BOOK_JOB_ID,
@@ -23,3 +26,15 @@ export class ChronogolfBookJob implements JobDefinition {
     };
   }
 }
+
+/*
+Associated job to run bookTime
+*/
+const bookTimeJob: FastifyPluginAsync = async (fastify: FastifyInstance) => {
+  fastify.jobTypes.set(CHRONOGOLF_BOOK_JOB_ID, new ChronogolfBookJob());
+};
+
+export default fp(bookTimeJob, {
+  name: CHRONOGOLF_BOOK_JOB_ID,
+  dependencies: ['agenda'],
+});
