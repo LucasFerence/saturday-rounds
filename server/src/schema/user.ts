3733a9c9854f5d332eaf78ed981aed99df31fce6
Lucas Ference <ference.lucas@gmail.com>
2023-06-16 21:13:48 -0400

    Add base ChronogolfProviderData type

--- server/src/schema/user.ts
+++ server/src/schema/user.ts
@@ -1,14 +1,14 @@
 import {Static, Type} from '@sinclair/typebox';
 import {DataDocument, SafeType, Schema} from './schema';
 
-export const ProviderDataType = Type.Object({});
+export const UserProviderDataType = Type.Object({});
 
-export class UserProviderData implements Static<typeof ProviderDataType> {}
+export type UserProviderData = Static<typeof UserProviderDataType>;
 
 export const UserType = SafeType({
   email: Type.String(),
   name: Type.Optional(Type.String()),
-  providerData: Type.Record(Type.String(), ProviderDataType),
+  providerData: Type.Record(Type.String(), UserProviderDataType),
 });
 
 export interface User extends Static<typeof UserType>, DataDocument {}
