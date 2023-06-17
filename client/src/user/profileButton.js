49de073ababa4748915ff5ee8bf954a8ae75ae11
CagedCrumpets <orkin.kyle@gmail.com>
2023-06-17 14:12:27 -0400

    made it look somewhat good with problems reducing screen size but problems are minor

--- client/src/user/profileButton.js
+++ client/src/user/profileButton.js
@@ -6,12 +6,29 @@ import UserProfile from './UserProfile';
 import {DashboardContext} from '../Dashboard';
 
 /**
+ * @param {var} variable
+ * @return{capitalized}
+ */
+function capitalizeFirstLetter(variable) {
+  if (typeof variable !== 'string') {
+    return variable; // Return the variable as is if it's not a string
+  }
+
+  if (variable.length === 0) {
+    return variable; // Return an empty string if the variable is empty
+  }
+
+  return variable.charAt(0).toUpperCase() + variable.slice(1);
+}
+
+/**
  * creates unstyled profile button
  * @return {button}
  */
 const UserButton = () => {
   const theme = core.useMantineTheme();
   const {user} = useAuth0();
+  const name = capitalizeFirstLetter(user?.nickname);
   const {setRenderedArea} = useContext(DashboardContext);
   const render = () => {
     return <UserProfile />;
@@ -22,7 +39,8 @@ const UserButton = () => {
         setRenderedArea(render);
       }}
       sx={{
-        height: '10vh',
+        height: 'fit-content',
+        padding: '10px, 0px',
         borderTop: `${core.rem(1)} solid ${
           theme.colorScheme === 'dark' ?
             theme.colors.brandGreen[4] :
@@ -35,11 +53,12 @@ const UserButton = () => {
           'display': 'block',
           'width': '100%',
           'padding': theme.spacing.xs,
-          'borderRadius': theme.radius.sm,
+          'borderRadius': theme.sm,
           'color':
             theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
 
           '&:hover': {
+            color: theme.white,
             backgroundColor:
               theme.colorScheme === 'dark' ?
                 theme.colors.brandGreenOne[7] :
@@ -51,20 +70,20 @@ const UserButton = () => {
           <core.Avatar
             src={user?.picture}
             radius="xl"
-            size={'3vw'}
+            size={'50px'}
           />
           <core.Box sx={{flex: 1}}>
-            <core.Text size="md" weight={500}>
-              {user?.name}
+            <core.Text size="17px" weight={550}>
+              {name}
             </core.Text>
-            <core.Text color="dimmed" size="xs">
+            <core.Text color='theme.grey' size="s" weight={100}>
               {user?.email}
             </core.Text>
           </core.Box>
           {theme.dir === 'ltr' ? (
-            <ArrowBigRightLine size={core.rem(18)} />
+            <ArrowBigRightLine size={core.rem(22)} />
           ) : (
-            <ArrowBigRightLine size={core.rem(18)} />
+            <ArrowBigRightLine size={core.rem(22)} />
           )}
         </core.Group>
       </core.UnstyledButton>
